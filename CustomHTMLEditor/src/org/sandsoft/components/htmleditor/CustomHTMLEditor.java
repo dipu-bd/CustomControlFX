769c20c5354bf7183dd85ed4f04642636355d692
dipu-bd <dipu.sudipta@gmail.com>
2015-11-21 00:25:01 +0600

    Can import any type of file now

--- CustomHTMLEditor/src/org/sandsoft/components/htmleditor/CustomHTMLEditor.java
+++ CustomHTMLEditor/src/org/sandsoft/components/htmleditor/CustomHTMLEditor.java
@@ -16,6 +16,7 @@
 package org.sandsoft.components.htmleditor;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.geometry.Orientation;
@@ -38,108 +39,120 @@ public class CustomHTMLEditor extends HTMLEditor {
     public static final String TOP_TOOLBAR = ".top-toolbar";
     public static final String BOTTOM_TOOLBAR = ".bottom-toolbar";
     public static final String WEB_VIEW = ".web-view";
-    private static final String IMPORT_BUTTON_IMAGE = "image.png";
+    private static final String IMPORT_BUTTON_GENERAL = "embed.png";
 
-    private WebView mWebView;
-    private ToolBar mTopToolBar;
-    private ToolBar mBottomToolBar;
-    private Button mImportImageButton;
+    private final WebView mWebView;
+    private final ToolBar mTopToolBar;
+    private final ToolBar mBottomToolBar;
+    private Button mImportFileButton;
 
     public CustomHTMLEditor() {
-        createCustomButtons();
-        this.setHtmlText("<html />");
-    }
-
-    private void createCustomButtons() {
-        //identify controls
         mWebView = (WebView) this.lookup(WEB_VIEW);
         mTopToolBar = (ToolBar) this.lookup(TOP_TOOLBAR);
         mBottomToolBar = (ToolBar) this.lookup(BOTTOM_TOOLBAR);
 
-        // add import image button
-        ImageView graphic = null;
-        try {
-            graphic = new ImageView(new Image(
-                    getClass().getResourceAsStream(IMPORT_BUTTON_IMAGE)));
-        } catch (Exception ex) {
-        }
-        mImportImageButton = new Button("Import Image", graphic);
-        mImportImageButton.setTooltip(new Tooltip("Import Image"));
-        mImportImageButton.setOnAction((event) -> {
-            onImportButtonAction();
-        });
+        createCustomButtons();
+        this.setHtmlText("<html />");
+    }
 
-        //add to top toolbar        
-        mTopToolBar.getItems().add(mImportImageButton);
+    /**
+     * Inserts HTML data after the current cursor. If anything is selected, they
+     * get replaced with new HTML data.
+     *
+     * @param html HTML data to insert.
+     */
+    public void insertHtmlAfterCursor(String html) {
+        //replace invalid chars
+        html = html.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\r", "\\r")
+                .replace("\n", "\\n");
+        //get script
+        String script = String.format(
+                "(function(html) {"
+                + "  var sel, range;"
+                + "  if (window.getSelection) {"
+                + "    sel = window.getSelection();"
+                + "    if (sel.getRangeAt && sel.rangeCount) {"
+                + "      range = sel.getRangeAt(0);"
+                + "      range.deleteContents();"
+                + "      var el = document.createElement(\"div\");"
+                + "      el.innerHTML = html;"
+                + "      var frag = document.createDocumentFragment(),"
+                + "        node, lastNode;"
+                + "      while ((node = el.firstChild)) {"
+                + "        lastNode = frag.appendChild(node);"
+                + "      }"
+                + "      range.insertNode(frag);"
+                + "      if (lastNode) {"
+                + "        range = range.cloneRange();"
+                + "        range.setStartAfter(lastNode);"
+                + "        range.collapse(true);"
+                + "        sel.removeAllRanges();"
+                + "        sel.addRange(range);"
+                + "      }"
+                + "    }"
+                + "  }"
+                + "  else if (document.selection && "
+                + "           document.selection.type != \"Control\") {"
+                + "    document.selection.createRange().pasteHTML(html);"
+                + "  }"
+                + "})(\"%s\");", html);
+        //execute script
+        mWebView.getEngine().executeScript(script);
+    }
+
+    /**
+     * Creates Custom ToolBar buttons and other controls
+     */
+    private void createCustomButtons() {
+        //add embed file button  
+        ImageView graphic = new ImageView(new Image(
+                getClass().getResourceAsStream(IMPORT_BUTTON_GENERAL)));
+        mImportFileButton = new Button("Import File", graphic);
+        mImportFileButton.setTooltip(new Tooltip("Import File"));
+        mImportFileButton.setOnAction((event) -> onImportFileButtonAction());
+
+        //add to top toolbar         
+        mTopToolBar.getItems().add(mImportFileButton);
         mTopToolBar.getItems().add(new Separator(Orientation.VERTICAL));
     }
 
-    private void onImportButtonAction() {
+    /**
+     * Action to do on Import Image button click
+     */
+    private void onImportFileButtonAction() {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select an image file");
-        FileChooser.ExtensionFilter imageFileFilter = new FileChooser.ExtensionFilter(
-                "Image Files", "*.png", "*.jpg", "*.bmp", "*.gif", "*.tif", "*.tiff");
-        FileChooser.ExtensionFilter allFileFilter = new FileChooser.ExtensionFilter(
-                "All Files", "*.*");
-        fileChooser.getExtensionFilters().add(imageFileFilter);
-        fileChooser.getExtensionFilters().add(allFileFilter);
-        fileChooser.setSelectedExtensionFilter(imageFileFilter);
+        fileChooser.setTitle("Select a file to import");
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("All Files", "*.*"));
         File selectedFile = fileChooser.showOpenDialog(this.getScene().getWindow());
         if (selectedFile != null) {
-            importImageFile(selectedFile);
+            importDataFile(selectedFile);
         }
     }
 
-    private void importImageFile(File file) {
+    /**
+     * Imports an image file.
+     *
+     * @param file Image file.
+     */
+    private void importDataFile(File file) {
         try {
-            //get type and alt-text
-            String fileName = file.getName();
-            String extension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-            String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf("."));
-
+            //check if file is too big
+            if (file.length() > 1024 * 1024) {
+                throw new VerifyError("File is too big.");
+            }
+            //get mime type of the file
+            String type = java.nio.file.Files.probeContentType(file.toPath());
             //get html content
             byte[] data = org.apache.commons.io.FileUtils.readFileToByteArray(file);
             String base64data = java.util.Base64.getEncoder().encodeToString(data);
             String htmlData = String.format(
-                    "<img src='data:image/%s;base64,%s' alt='%s'>",
-                    extension, base64data, fileNameWithoutExtension);
-
-            //insert beside cursor
-            String script = String.format("(function(html) {\n"
-                    + "    var sel, range;\n"
-                    + "    if (window.getSelection) {\n"
-                    + "        // IE9 and non-IE\n"
-                    + "        sel = window.getSelection();\n"
-                    + "        if (sel.getRangeAt && sel.rangeCount) {\n"
-                    + "            range = sel.getRangeAt(0);\n"
-                    + "            range.deleteContents();\n"
-                    + "            // Range.createContextualFragment() would be useful here but is\n"
-                    + "            // only relatively recently standardized and is not supported in\n"
-                    + "            // some browsers (IE9, for one)\n"
-                    + "            var el = document.createElement(\"div\");\n"
-                    + "            el.innerHTML = html;\n"
-                    + "            var frag = document.createDocumentFragment(), node, lastNode;\n"
-                    + "            while ( (node = el.firstChild) ) {\n"
-                    + "                lastNode = frag.appendChild(node);\n"
-                    + "            }\n"
-                    + "            range.insertNode(frag);\n"
-                    + "            // Preserve the selection\n"
-                    + "            if (lastNode) {\n"
-                    + "                range = range.cloneRange();\n"
-                    + "                range.setStartAfter(lastNode);\n"
-                    + "                range.collapse(true);\n"
-                    + "                sel.removeAllRanges();\n"
-                    + "                sel.addRange(range);\n"
-                    + "            }\n"
-                    + "        }\n"
-                    + "    } else if (document.selection && document.selection.type != \"Control\") {\n"
-                    + "        // IE < 9\n"
-                    + "        document.selection.createRange().pasteHTML(html);\n"
-                    + "    }\n"
-                    + "})(\"%s\");", htmlData);
-
-            mWebView.getEngine().executeScript(script);
-        } catch (Exception ex) {
+                    "<embed src='data:%s;base64,%s' type='%s' />",
+                    type, base64data, type);
+            //insert html
+            insertHtmlAfterCursor(htmlData);
+        } catch (IOException ex) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
         }
     }
